408bc082da5ec13a5644c46e86a98189a0f8bbe6
dylan <dylan@localhost>
2023-09-30 15:31:47 -0700

    first commit to repo.

---
+++ Lab02/src/lab02/Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		System.out.println("This line was added by Dylan Pierre");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
+
+}

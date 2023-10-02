d34c5133dde85fe51bad93886a7c4d99faf0aae3
yahir <yahir@localhost>
2023-10-02 11:10:44 -0700

    Line 10 commit

--- Lab02/src/lab02/Hello.java
+++ Lab02/src/lab02/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
                 //Insert each of your names below here. Then commit, and push.
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First Mojtaba Last Karanizadeh.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Yahir Ramos Perez.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");

b877783faecd499051e12f3ca971565e34c9a27c
dylan <dylan@localhost>
2023-10-02 10:59:57 -0700

    first commit to repo.

--- Lab02/src/lab02/Hello.java
+++ Lab02/src/lab02/Hello.java
@@ -1,10 +1,10 @@
 
 public class Hello {
-
+  
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
-		System.out.println("yang - This line was added by Dr. Yang.");
 		System.out.println("This line was added by Dylan Pierre");
+                //Insert each of your names below here. Then commit, and push.
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First Mojtaba Last Karanizadeh.");
 		System.out.println("SacStateID - This line was added by First name Last name.");

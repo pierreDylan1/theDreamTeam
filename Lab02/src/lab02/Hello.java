94ec91651f5e2669f679cf082e324a05dfc96d3e
dylan <dkeener@csus.edu>
2023-10-02 19:14:39 -0700

    Added name

--- Lab02/src/lab02/Hello.java
+++ Lab02/src/lab02/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by Kwin Lee.");
 		System.out.println("SacStateID - This line was added by Mitchell Kouiyoth.");
 		System.out.println("SacStateID - This line was added by Isaac Thawer.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Dylan Keener (1).");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 

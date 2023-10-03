5b8c18229199385add52d6792f53c687da7f359f
mk <mk@mojtabas-mbp.wavecable.com>
2023-10-02 18:46:07 -0700

    Just updated my name

--- Lab02/src/lab02/Hello.java
+++ Lab02/src/lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("This line was added by Dylan Pierre");
                 //Insert each of your names below here. Then commit, and push.
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First Mojtaba Last Karanizadeh.");
+		System.out.println("SacStateID - This line was added by Mojtaba Karanizadeh.");
 		System.out.println("SacStateID - This line was added by Yahir Ramos Perez.");
 		System.out.println("SacStateID - This line was added by Kwin Lee.");
 		System.out.println("SacStateID - This line was added by Mitchell Kouiyoth.");

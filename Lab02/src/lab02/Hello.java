e749c43b8b0452a4a70999825a79a533e1c4ed2b
dylan <dylan@localhost>
2023-10-03 20:05:41 -0700

    final commit to repo. Completed Lab

--- Lab02/src/lab02/Hello.java
+++ Lab02/src/lab02/Hello.java
@@ -4,16 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("This line was added by Dylan Pierre");
-                //Insert each of your names below here. Then commit, and push.
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by Mojtaba Karanizadeh.");
 		System.out.println("SacStateID - This line was added by Yahir Ramos Perez.");
 		System.out.println("SacStateID - This line was added by Kwin Lee.");
 		System.out.println("SacStateID - This line was added by Mitchell Kouiyoth.");
 		System.out.println("SacStateID - This line was added by Isaac Thawer.");
 		System.out.println("SacStateID - This line was added by Dylan Keener.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 
 }
 
